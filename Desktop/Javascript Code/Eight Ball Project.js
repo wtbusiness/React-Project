97bde6cb1c28b9d879902fc1a16f75424934ca59
Wayne <waynetuckermedia@gmail.com>
2022-07-03 07:03:22 -0400

    new edit 7-3-2022

--- Desktop/Javascript Code/Eight Ball Project.js
+++
@@ -1,9 +0,0 @@
-let userName = ""
-userName ? console.log(`Hello, ${userName}!`) : console.log("Hello")
-
-let userQuestion = ""
-console.log (userQuestion)
-
-let randomNumber = Math.floor() * 8;
-
-let eightBall = ""
