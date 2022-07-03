060455b6e043d5cf42a8329fa351cff5a6210a78
Wayne <waynetuckermedia@gmail.com>
2022-07-03 06:21:42 -0400

    7-3-2022 update

---
+++ Desktop/Ultra-Questionaire-main/src/App.js
@@ -0,0 +1,35 @@
+import {v4 as uuidv4} from 'uuid'
+import {useState} from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackData from './data/FeedbackData'
+
+
+function App() {
+  const [feedback, setFeedback ] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter((item) =>item.id !== id))
+    }
+  }
+  return (
+    <>
+    <Header  />
+    <div className="container">
+      <FeedbackForm handleAdd={addFeedback} />
+      <FeedbackStats feedback={feedback}  />
+       <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ Desktop/Ultra-Questionaire-main/src/components/FeedbackList.jsx
@@ -0,0 +1,25 @@
+import {} from 'framer-motion'
+import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({feedback, handleDelete}) {
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback Yet</p>
+
+    }
+  return (
+  <div className='feedback-list'>
+        {feedback.map((item) => (
+         <FeedbackItem
+          key={item.id}
+           item={item}
+         handleDelete={handleDelete} /> 
+
+        ))}
+    </div>
+  )
+}
+FeedbackList.propTypes = {
+    feedback: PropTypes.array
+}
+export default FeedbackList
